9db654916e922dbfe739f9deb65db3b227cbd839
William Blacknall <williamblacknall@gmail.com>
2019-11-07 09:48:57 -0500

    Added more props to calling comment information.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
 //Import the React and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom";
-import CommentDetail from "./CommentDetail"
+import CommentDetail from "./CommentDetail";
+import Faker from "faker";
 
 // Create a react component
 const App = function () {
-    return (
-        <div className="ui container comments">
-            <CommentDetail author="Sam" />
-            <CommentDetail author="Alex" />
-            <CommentDetail author="Jane"/>
+    return ( 
+        <div className = "ui container comments" >
+        <CommentDetail 
+        author = "Sam"
+        timeAgo = "Today at 4:45PM"
+        commentText = "Sometimes I just really want to eat birds. Not dead ones, but live ones."
+        avatar = {Faker.image.avatar()}
+        /> 
+        <CommentDetail
+        author = "Alex"
+        timeAgo = "Today at 2:00AM"
+        commentText = "I know that feel."
+        avatar = {Faker.image.avatar()}
+        /> 
+        <CommentDetail
+        author = "Jane"
+        timeAgo = "Today at 5:15PM"
+        commentText = "Sweet lord. What?"
+        avatar = {Faker.image.avatar()}
+        /> 
         </div>
-)};
+    );
+};
 
 
 //Take the react component and show it on the screen
-ReactDOM.render( <App /> , document.querySelector('#root'))
+ReactDOM.render( < App /> , document.querySelector('#root'))
